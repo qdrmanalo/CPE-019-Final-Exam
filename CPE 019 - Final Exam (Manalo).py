b20ec5d6d7357c8c53e98e2bc5893f38dcc153fc
qdrmanalo <133857175+qdrmanalo@users.noreply.github.com>
2023-05-17 18:36:42 +0800

    Update CPE 019 - Final Exam (Manalo).py

--- CPE 019 - Final Exam (Manalo).py
+++ CPE 019 - Final Exam (Manalo).py
@@ -21,11 +21,12 @@ import cv2
 from PIL import Image,ImageOps
 import numpy as np
 def import_and_predict(image_data,model):
-    size=(32,32,3)
+    size=(32,32)
     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
     img=np.asarray(image)
     img=cv2.resize(img,size,cv2.INTER_CUBIC)
-    prediction=model.predict(img)
+    img_reshape = img.reshape((1,),img.shape,(1,))
+    prediction=model.predict(img_reshape)
     return prediction
  
 if file is None:
